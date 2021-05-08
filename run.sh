a04ad1188bb2f25e1b2a00769ebf41627ac6e169
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2021-05-08 14:47:25 -0400

    Fix issue with docker container not persisting reports

--- run.sh
+++ run.sh
@@ -1,11 +1,26 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]
+then
+	echo "Invalid arguments supplied!"
+	echo ""
+	echo "Usage:"
+	echo "  ./run.sh <container name> <token> <database directory>"
+	exit 1
+fi
+
+CONTAINER_NAME="$1"
+TOKEN="$2"
+DB_DIR="$3"
+
 docker build . -t narc
-docker container stop narc
-docker container rm narc
+docker container stop "$CONTAINER_NAME"
+docker container rm "$CONTAINER_NAME"
 docker run \
 	--detach \
-	--name narc \
-	-e "DATABASE_URL=sqlite:narc.db" \
+	--name "$CONTAINER_NAME" \
+	-e "DATABASE_URL=sqlite:/db/narc.db" \
 	-e "DISCORD_TOKEN=$TOKEN" \
 	-e "RUST_LOG=narc=warn" \
-	-v "$PWD/narc.db":/app/narc.db \
+	-v "$DB_DIR":/db \
 	narc
